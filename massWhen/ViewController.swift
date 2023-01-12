d98e582193f88f11c133b96c14989ccf12f3d838
ehdwns11 <ehdwns7339@naver.com>
2023-01-12 13:47:39 +0900

    Initial Commit

---
+++ massWhen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  massWhen
+//
+//  Created by 동준북 on 2023/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Do something
+    }
+
+
+}
+
